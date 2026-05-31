b8f788858668d0235df53f0603bbf522f2ee171e
kkelly-27 <kkelly27@gcds.net>
2026-05-31 18:42:48 -0400

    Update app.py

--- CSWeb/app.py
+++ CSWeb/app.py
@@ -7,19 +7,21 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['GET'])
 def submit():
 
-    name = request.form['name']
-    grade = request.form['grade']
-    major = request.form['major']
+    name = request.args.get('name')
+    grade = request.args.get('grade')
+    major = request.args.get('major')
 
     with open('responses.csv', 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow([name, grade, major])
 
-    return "Response Saved!"
-
+    return """
+    <h2>Response Saved!</h2>
+    <a href="/">Back to Survey</a>
+    """
 
 if __name__ == '__main__':
     app.run(debug=True)
